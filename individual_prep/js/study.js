919f3632c600f808d3734de5456aad8c074598e5
RobertNiyonkuru98 <r.niyonkuru@alustudent.com>
2025-10-03 11:27:44 +0200

    Individual prep

---
+++ individual_prep/js/study.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+console.log("Test");
+
+/*Variables */
+var variableName = "Welcome to Variables";
+const variableNom = "CANNOT BE REASSIGNED";
+console.log(variableName);
+console.log(variableNom);
