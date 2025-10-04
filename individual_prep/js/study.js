71182458cf9b6ed3423e6bd87625df8addccb747
Robert <r.niyonkuru@alustudent.com>
2025-10-04 21:10:47 +0200

    Individual prep

--- individual_prep/js/study.js
+++ individual_prep/js/study.js
@@ -1,8 +1,145 @@
+/*
 console.log("Hello World!");
 console.log("Test");
+*/
 
 /*Variables */
+
+//let  //const(no reassignment)
+//var
+
+/*
 var variableName = "Welcome to Variables";
 const variableNom = "CANNOT BE REASSIGNED";
+const name = "john";
+const _nom = "Johnny";
+const $alias = "Johnboy";
+let person = "Tony";
 console.log(variableName);
 console.log(variableNom);
+console.log(_nom);
+console.log($alias);
+console.log(name);
+console.log(person);
+
+*/
+
+/*Data Types*/
+
+//Strings  //Booleans  //Undefined  //Symbol
+//Numbers  //Null      //Object
+
+// String
+
+/*
+const exampleString = "How are you doing";
+const singleQuotes = "This is a single quoted string";
+const doubleQuotes = "This is a double quoted string";
+const backticks = `This is backtick quoted string`;
+const name = "Jane";
+const nameTicks = `Hello ${name}, welcome!`;
+const numberTicks = `2 + 2 = ${2 + 2}`;
+console.log(typeof numberTicks);
+console.log(numberTicks);
+console.log(nameTicks);
+console.log(backticks);
+console.log(doubleQuotes);
+console.log(singleQuotes);
+console.log(exampleString);
+console.log("Hello world");
+
+*/
+
+// Numbers
+
+/*
+const wholeNumber = 5;
+const decimalNumber = 0.5;
+const firstNumber = 5;
+const secondNumber = 10;
+const add = firstNumber + secondNumber;
+const minus = firstNumber - secondNumber;
+const multiply = firstNumber * secondNumber;
+const divide = firstNumber / secondNumber;
+console.log(typeof wholeNumber);
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
+console.log(wholeNumber);
+console.log(decimalNumber);
+
+*/
+
+// Booleans
+
+/*
+const isCool = true;
+const age = 20;
+console.log(age > 20);
+console.log(typeof isCool);
+console.log(isCool);
+
+if (isCool) {
+  console.log("Yo man, you're cool");
+} else {
+  console.log("Oh, Hi");
+}
+
+*/
+
+// NULL
+
+/*
+const age = null;
+let year = null;
+year = 20;
+console.log(age);
+console.log(year);
+
+*/
+
+// UNDEFINED
+
+/*
+let x;
+console.log(x);
+
+*/
+
+// OBJECTS
+
+/*
+const name = "John";
+const age = 25;
+const person = {
+  name: "John",
+  age: 25,
+};
+
+console.log(person);
+*/
+
+// Dot notation
+
+/*console.log(person.name);*/
+
+// Array
+
+/*
+const arr = [1, 2, 3, 4];
+const date = new Date();
+console.log(date);
+console.log(arr);
+
+*/
+
+// Statically typed (C)
+// Dynamically typed (JS)
+
+/*
+let message = "Hello worlds";
+message = 5;
+console.log(message);
+
+*/
