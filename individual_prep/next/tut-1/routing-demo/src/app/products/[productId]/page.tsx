a2b8c28fc8b5413a56676ccba05666530ac18be9
Robert <r.niyonkuru@alustudent.com>
2025-12-03 22:04:40 +0200

    Individual Prep

--- individual_prep/next/tut-1/routing-demo/src/app/products/[productId]/page.tsx
+++ individual_prep/next/tut-1/routing-demo/src/app/products/[productId]/page.tsx
@@ -1,14 +1,8 @@
 export default async function productDetails({
   params,
 }: {
-  params: Promise<{ productId: string; name: string }>;
+  params: Promise<{ productId: string }>;
 }) {
   const productId = (await params).productId;
-  const MyName = (await params).name;
-  return (
-    <>
-      <h1>Details about product {productId}</h1>
-      <h1>My name is {MyName}</h1>;
-    </>
-  );
+  return <h1>Details about product {productId}</h1>;
 }
